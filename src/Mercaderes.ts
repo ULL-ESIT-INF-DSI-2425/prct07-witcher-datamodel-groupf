d826e0daeb8b9d3118e13ee189411289c72836c9
Adrian Leon Diaz <alu0101495668@ull.edu.es>
2025-03-16 18:27:51 +0000

    Falta las dos ultimas clases

---
+++ src/Mercaderes.ts
@@ -0,0 +1,85 @@
+
+/**
+ * Clase que representa a cada Mercader
+ */
+export class Mercader {
+    /**
+     * Constructor de la calse Mercader
+     * @param _id - id unico del mercader
+     * @param _nombre - Nombre del mercader
+     * @param _tipo - Tipo de oficio del mercader
+     * @param _ubicacion - Lugar donde se encuentra el mercader
+     */
+    constructor(
+        private _id: string,
+        private  _nombre: string,
+        private _tipo: string,
+        private _ubicacion: string,
+    ) {}
+
+
+    /**
+     * Getter que devuelve  el id del Mercader
+     * @returns idUnico del mercader
+     */
+    get idUnico(): string {
+        return this._id;
+    }
+
+
+    /**
+     * Getter qie devuelve el nombre del mercader
+     * @returns nombre del mercader
+     */
+    get nombre(): string {
+        return this._nombre;
+    }
+
+    /**
+     * Getter que devuelve el tipo de oficio del mercader
+     * @returns tipo de oficio
+     */
+    get tipo(): string {
+        return this._tipo;
+    }
+
+    /**
+     * Getter que devuelve la ubicacion de donde esta el mercader
+     * @returns ubciacion del mercader
+     */
+    get ubicacion(): string {
+        return this.ubicacion;
+    }
+
+    /**
+     * Setter que modifica el valor del id del mercader
+     * @param id del mercader
+     */
+    set idUnico(id: string) {
+        this._id = id;
+    }
+    
+     /**
+     * Setter que modifica el valor del nombre del mercader
+     * @param nombre, nombre del mercader
+     */
+    set nombre(nombre: string) {
+        this._nombre = nombre;
+    }
+
+    /**
+     * Setter que modifica el valor del tipo de oficio del mercader
+     * @param tipo, oficio del mercader
+     */
+    set tipo(tipo: string) {
+        this._tipo = tipo;
+    }
+
+    /**
+     * Setter que modifica la ubicacion del mercader
+     * @param ubicacion es la ubicacion del mercader
+     */
+    set ubicacion(ubicacion: string) {
+        this._ubicacion = ubicacion;
+    }
+}
---
+++ src/cliente.ts
@@ -0,0 +1,88 @@
+
+
+/**
+ * Clase que reprsenta a todas las criaturas que buscan herramientas
+ */
+export class Cliente {
+    /**
+     * Constructor de la clase cliente
+     * @param _idUnico - idUnico del cliente
+     * @param _nombre - nombre del cliente
+     * @param _raza - raza del cliente
+     * @param _ubicacion - ubicacion del cliente
+     */
+    constructor(
+        private _idUnico: string,
+        private _nombre: string,
+        private _raza: string,
+        private _ubicacion: string,
+    ) {}
+
+    // Getter
+
+    /**
+     * Getter que devuelve el idUnico del cliente
+     * @returns - idUnico del cliente
+     */
+    get idUnico(): string {
+        return this._idUnico;
+    }
+
+    /**
+     * Getter que devuelve el nombre del cliente
+     * @returns - nombre del cliente
+     */
+    get nombre(): string {
+        return this._nombre;
+    }
+
+    /**
+     * Getter que devuelve la raza del cliente
+     * @returns - raza del cliente
+     */
+    get raza(): string {
+        return this._nombre;
+    }
+    
+    /**
+     * Getter que devuelve la ubicacion de donde esta el cliente
+     * @returns ubciacion del cliente
+     */
+    get ubicacion(): string {
+        return this._ubicacion;
+    }
+
+    // setter
+
+     /**
+     * Setter que establece el idUnico del cliente
+     * @param idUnico - idUnico del cliente
+     */
+     set idUnico(idUnico: string) {
+        this._idUnico = idUnico;
+    }
+    
+     /**
+     * Setter que establece el nombre del cliente
+     * @param nombre - nombre del cliente
+     */
+     set nombre(nombre: string) {
+        this._nombre = nombre;
+    }
+
+    /**
+     * Setter que estalece la raza del cliente
+     * @retruns - raza del cliente
+     */
+    set raza(raza: string) {
+        this._raza = raza;
+    }
+
+      /**
+     * Setter que establece la ubicacion del cliente
+     * @param - ubicacion del cliente
+     */
+      set ubicacion(ubicacion: string) {
+        this._ubicacion = ubicacion;
+    }
+}
